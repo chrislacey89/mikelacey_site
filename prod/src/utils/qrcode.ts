36de3182185cc4a45c2283c776d2c86f3d5f1dda
Chris Lacey <chrislacey89@gmail.com>
2025-12-26 10:30:46 -0500

    Increase QR code size to 400px and add high error correction level for better scanning reliability

--- prod/src/utils/qrcode.ts
+++ prod/src/utils/qrcode.ts
@@ -5,8 +5,10 @@ import { generateVCard } from './vcard';
 export async function generateVCardQR(contact: ContactInfo): Promise<string> {
   const vcard = generateVCard(contact);
   return QRCode.toDataURL(vcard, {
-    width: 200,
+    type: 'image/png',
+    width: 400,
     margin: 2,
+    errorCorrectionLevel: 'H',
     color: {
       dark: '#292524', // stone-800
       light: '#ffffff'
