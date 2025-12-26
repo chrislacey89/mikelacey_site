f3f1fc1660a6e2a90be482cec0f61270a03a7a4c
Chris Lacey <chrislacey89@gmail.com>
2025-12-26 15:30:32 -0500

    Update site URL to include www subdomain in Astro config and BaseLayout fallback

--- prod/astro.config.mjs
+++ prod/astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://themikelacey.com',
+  site: 'https://www.themikelacey.com',
   output: 'static',
   adapter: vercel(),
   vite: {
